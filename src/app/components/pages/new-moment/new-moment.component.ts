ccf49e47d84df9690053ec249f5691db8cc56a04
Rosemberg Bezerra <rosemberg.bezerra-ext@jci.com>
2024-04-13 10:10:41 +0100

    moment project

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent implements OnInit {
+
+btnText: string = "Compartilhar"
+
+  constructor(private momentService: MomentService) { }
+
+  ngOnInit(): void {
+  }
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData()
+
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+
+   
+      formData.append("image", moment.image)
+    
+
+    await this.momentService.createMoment(formData).subscribe();
+  }
+
+}
